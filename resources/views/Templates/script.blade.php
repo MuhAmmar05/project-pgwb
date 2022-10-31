78f7b3d83e8f7e948bc1bf8ff2a9bd2f0fd1cfd4
MuhAmmar05 <88220052+MuhAmmar05@users.noreply.github.com>
2022-10-31 13:14:22 +0700

    Update script.blade.php

--- resources/views/Templates/script.blade.php
+++ resources/views/Templates/script.blade.php
@@ -26,8 +26,8 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="template/vendor/jquery/jquery.min.js"></script>
-    <script src="template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="Template/vendor/jquery/jquery.min.js"></script>
+    <script src="Template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
     <script src="Template/vendor/jquery-easing/jquery.easing.min.js"></script>
